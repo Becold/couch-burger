e4f712c15e5efc714aa9b6577e1db1c0ef7e10c3
Romain Marbaix <contact@romainmarbaix.be>
2018-12-10 19:47:01 +0100

    début du controlleur des produits

---
+++ src/src/main/java/com/spring/henallux/templatesSpringProject/controller/ProductController.java
@@ -0,0 +1,34 @@
+package com.spring.henallux.templatesSpringProject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping(value = "/product")
+public class ProductController {
+
+    /*
+    private ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+     */
+
+    @RequestMapping(value = "/product/{productId}",
+            method = RequestMethod.GET)
+    public String getProduct(Model model,
+                             @PathVariable("productId") int id) {
+        /*
+        Product product = productService.find(id);
+        model.addAttribute("product", product);
+         */
+        model.addAttribute("title", "Article");
+        return "integrated:product"; // TODO Template category.jsp
+    }
+}
