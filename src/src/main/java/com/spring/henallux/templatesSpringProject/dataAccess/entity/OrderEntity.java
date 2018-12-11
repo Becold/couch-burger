81dec37e17fad8d31f513d537f308c8a30eada45
Romain Marbaix <contact@romainmarbaix.be>
2018-12-11 18:04:52 +0100

    creation de l'entité Order

--- src/src/main/java/com/spring/henallux/templatesSpringProject/dataAccess/entity/OrderEntity.java
+++ src/src/main/java/com/spring/henallux/templatesSpringProject/dataAccess/entity/OrderEntity.java
@@ -1,4 +1,26 @@
 package com.spring.henallux.templatesSpringProject.dataAccess.entity;
 
+import com.spring.henallux.templatesSpringProject.model.User;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.GregorianCalendar;
+
 public class OrderEntity {
+    @Id
+    @GeneratedValue
+    @Column(name = "orderId")
+    private Integer orderId;
+
+    @Column(name = "user_id")
+    private User user;
+
+    @Column(name = "creationDate")
+    private GregorianCalendar creationDate;
+
+    @Column(name = "isPaid")
+    private Boolean isPaid;
+
+    public OrderEntity() { }
 }
