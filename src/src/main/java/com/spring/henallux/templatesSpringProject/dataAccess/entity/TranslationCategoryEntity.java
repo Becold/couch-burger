1da7b501a68c7575b47ad5fda5e889539284ea11
Romain Marbaix <contact@romainmarbaix.be>
2018-12-11 18:07:08 +0100

    ajout entité TranslationCategory

---
+++ src/src/main/java/com/spring/henallux/templatesSpringProject/dataAccess/entity/TranslationCategoryEntity.java
@@ -0,0 +1,24 @@
+package com.spring.henallux.templatesSpringProject.dataAccess.entity;
+
+import com.spring.henallux.templatesSpringProject.model.Category;
+
+import javax.persistence.Column;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+public class TranslationCategoryEntity {
+    @Column
+    private Integer translationId;
+
+    @ManyToOne
+    @JoinColumn(name = "categoryId")
+    private Category category;
+
+
+    @ManyToOne
+    @JoinColumn(name = "languageId")
+    private LanguageEntity language;
+
+    @Column
+    private String content;
+}
