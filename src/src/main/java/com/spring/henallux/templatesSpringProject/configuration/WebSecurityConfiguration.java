d26601eac42a8525d1bfafabc1da680f19bb97d0
Romain Marbaix <contact@romainmarbaix.be>
2018-12-19 11:38:36 +0100

    update

--- src/src/main/java/com/spring/henallux/templatesSpringProject/configuration/WebSecurityConfiguration.java
+++ src/src/main/java/com/spring/henallux/templatesSpringProject/configuration/WebSecurityConfiguration.java
@@ -32,6 +32,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
             "/webfonts/**",
 
             "/register",
+            "/login",
 
             "/cart/**",
             "/category/**",
