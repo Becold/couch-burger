c7d8848b2760025749ddb27e8f23edd2f3b67423
etu33987 <etu33987@student.henallux.be>
2018-12-19 14:08:14 +0100

    Traduction error

--- src/src/main/java/com/spring/henallux/templatesSpringProject/controller/RegisterUserController.java
+++ src/src/main/java/com/spring/henallux/templatesSpringProject/controller/RegisterUserController.java
@@ -58,7 +58,6 @@ public class RegisterUserController {
 
         if (!userRegister.getPassword().equals(userRegister.getConfirmPassword())) {
             errors.rejectValue("confirmPassword", "invalid.register.differentPassword");
-            errors.rejectValue("confirmPassword", "invalid.register.differentPassword");
         }
 
         if (this.userService.existsByUsername(userRegister.getUsername())) {
