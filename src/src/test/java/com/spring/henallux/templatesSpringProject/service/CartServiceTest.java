58e0b5a084ec48a110038b20f064eb04b66c48a2
Romain Marbaix <contact@romainmarbaix.be>
2019-01-12 18:39:32 +0100

    update

--- src/src/test/java/com/spring/henallux/templatesSpringProject/service/CartServiceTest.java
+++ src/src/test/java/com/spring/henallux/templatesSpringProject/service/CartServiceTest.java
@@ -1,21 +1,55 @@
 package com.spring.henallux.templatesSpringProject.service;
 
+import com.spring.henallux.templatesSpringProject.model.Product;
+import com.spring.henallux.templatesSpringProject.model.Promotion;
+import com.spring.henallux.templatesSpringProject.model.form.cart.ProductCart;
+import com.spring.henallux.templatesSpringProject.model.promotion.FinalAmountCart;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 import static org.junit.Assert.*;
 
 public class CartServiceTest {
 
+    private CartService cartService;
+
     @Before
-    public void setUp() throws Exception {
+    @Autowired
+    public void setUp(CartService cartService) throws Exception {
+        this.cartService = cartService;
     }
 
     @Test
-    public void findBestPromotionForProduct() {
+    public void findBestPromotionForProductTest() {
+        Product product = new Product();
+        // TODO Faire tous les setters pour créer un faux produit
+
+        ArrayList<Promotion> promotions = new ArrayList<Promotion>();
+        // TODO Remplir la array list de fausse promotions
+
+        Double reductionAmount = this.cartService.findBestPromotionForProduct(product, promotions);
+
+        // TODO A la place de 5.01, mettre le montant de la reduction calculé ci-dessus
+        // TODO 0.01 correspond, à changer peut-être
+        Assert.assertEquals(5.01, reductionAmount, 0.01);
     }
 
     @Test
-    public void getFinalAmountCart() {
+    public void getFinalAmountCartTest() {
+        HashMap<Integer, ProductCart> cart = new HashMap<>();
+        // TODO Remplir le carte de ProductCart (qui contient des quantités et des produits...
+        // TODO Donc créer des produits avant ça
+
+        FinalAmountCart finalAmountCart = this.cartService.getFinalAmountCart(cart);
+
+        // TODO A la place de 5.01, mettre le montant de la reduction calculé ci-dessus
+        // TODO 0.01 correspond, à changer peut-être
+        Assert.assertEquals(5.01, finalAmountCart.getTotal(), 0.01);
+        Assert.assertEquals(5.01, finalAmountCart.getReduction(), 0.01);
     }
 }
